b4e4f90d59c62c8d309020aa7ab29701a2dc3a72
deok94 <ejrwns1004@naver.com>
2019-01-15 14:14:54 +0900

    Candidate_key Commit - not yet

---
+++ Candidate_Key.py
@@ -0,0 +1,25 @@
+relation = [[100,"ryan","music",2],[200,"apeach","math",2],[300,"tube","computer",3],[400,"con","computer",4],[500,"muzi","music",3],[600,"apeach","music",2]]
+result = 2
+
+dic_test = {}
+# dic_test['100'] = '0'
+# dic_test['100'] = '0'
+# dic_test['100'] = '0'
+# dic_test['100'] = '0'
+# dic_test[100] = 0
+rl = []
+
+for i in range(len(relation)) :
+    rl_2 = []
+    for y in range(len(relation[i])):
+
+        rl.append(relation[i][y])
+print(rl)
+
+print(dic_test.items())
+# for i,y in dic_test.items():
+#     print(str(i),str(y))
+
+# for i in relation :
+#     for y in i:
+#         print()
